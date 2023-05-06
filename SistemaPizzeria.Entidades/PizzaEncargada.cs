fc2b12ad622dbb59f34169273cfcecb13f0a56d9
Horacio Capdevila <61241842+hor-5@users.noreply.github.com>
2023-05-06 00:21:52 -0300

    swagger toppings selected updates

--- SistemaPizzeria.Entidades/PizzaEncargada.cs
+++ SistemaPizzeria.Entidades/PizzaEncargada.cs
@@ -1,7 +1,12 @@
 ﻿namespace SistemaPizzeria.Entidades
 {
-    public class PizzaEncargada : Pizza
-    {        
-        public List<Topping>? toppingsSeleccionados;
+    public class PizzaEncargada 
+    {
+        public int IdPizza { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+        public int Size { get; set; }
+        public double Price { get; set; }
+        public List<Topping>? toppingsSeleccionados { get; set; }
     }
 }
